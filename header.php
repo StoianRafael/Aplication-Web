1272ba5a9d0b17f14dc8363ca66753b5e93b881b
StoianRafael <48418611+StoianRafael@users.noreply.github.com>
2019-03-12 13:58:19 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,30 +1,30 @@
 
     <div class="header">
-        <div class="logo_Rafael">
+        <div class="logo">
         <a href="home.php">
-            <img class="left_Rafael" src="image/logo.png" alt=" " width="300px" />
+            <img class="left" src="image/logo.png" alt=" " width="300px" />
             </a>
         </div>
-        <div class="search_Rafael">
-            <div class="search-box_Rafael">
-                <div class="text_Rafael">
-                    <input class="search-txt_Rafael" type="text" name=" " />
+        <div class="search">
+            <div class="search-box">
+                <div class="text">
+                    <input class="search-txt" type="text" name=" " />
                 </div>
-                <div class="search-btn_Rafael">
+                <div class="search-btn">
                     <a href="">
                         <img src="image/musica-searcher.png" alt=" " />
                     </a>
                 </div>
             </div>
         </div>
-        <div class="right_Rafael">
-            <div class="image_Rafael image_bulb_Rafael">
-                <img class="jos_Rafael border_icon_Rafael" src="image/light-bulb1.png" alt=" " />
+        <div class="right">
+            <div class="image image_bulb">
+                <img class="jos border_icon" src="image/light-bulb1.png" alt=" " />
             </div>
-            <div class="image_Rafael image_user_Rafael">
-                <img class="jos_Rafael border_icon_Rafael" src="image/user.png" alt=" " />
+            <div class="image image_user">
+                <img class="jos border_icon" src="image/user.png" alt=" " />
             </div>
-            <div class="user_Rafael">
+            <div class="user">
            <?php
             if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
                  include 'login_user/username_connected.php'; 
